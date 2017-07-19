8eaac961527fecaf54b0c70998c64ddcb81350fb
cptbrkfst <dyl@resonategroup.ca>
2017-07-19 19:16:11 -0400

    changed table to populate terminal-table

--- lib/canadian_mergers/merger_list.rb
+++ lib/canadian_mergers/merger_list.rb
@@ -1,18 +1,16 @@
-# Instantiate mergers from list
+# Instantiate list from merger objects
 class CanadianMergers::MergerList
 
   def self.table
-    # Fills table with merger objects
-    puts <<-DOC
-
-    |           Parties to the Transaction           | Industry |  Result |
-    | --------------------------------------------------------------------|
-    | Acklands-Grainger Inc. / WFS Enterprises Ltd.  |   4189   |   NAL   |
-    | --------------------------------------------------------------------|
-    | AECOM Technology Corporation / URS Corporation |   5413   |   NAL   |
-    | --------------------------------------------------------------------|
-
-    DOC
+    # Mocks table of merger objects
+    # Constructs table from array of merger objects using terminal-table
+    headings = ['Parties to Transaction', 'Industry', 'Result']
+    table = Terminal::Table.new headings: headings do |t|
+      t.add_row ['Acklands-Grainger Inc. / WFS Enterprises Ltd.', 4189, 'NAL']
+      t.add_row ['AECOM Technology Corporation / URS Corporation', 5413, 'NAL']
+      t.style = { all_separators: true, alignment: :center }
+    end
+    puts table
   end
 
 end
