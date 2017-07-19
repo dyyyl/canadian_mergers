39b9daf5e0e21630dbe42bbfbf1bf71c8a4ea8fd
cptbrkfst <dyl@resonategroup.ca>
2017-07-18 20:29:31 -0400

    stubbed initial menu functionality

--- lib/canadian_mergers/cli.rb
+++ lib/canadian_mergers/cli.rb
@@ -1,8 +1,22 @@
 # CLI Controller
 class CanadianMergers::CLI
+  attr_accessor :year, :month
 
   def call
+    menu
     puts 'Merger reviews concluded for the period:'
+    list_mergers(:year, :month)
+  end
+
+  def menu
+    puts 'Please enter the year you would like to access: '
+    @year = gets.strip
+    puts 'Please enter the month you would like to access:'
+    @month = gets.strip
+  end
+
+  def list_mergers(year, month)
+    # list mergers from scraped data
   end
 
 end
