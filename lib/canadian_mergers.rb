e8c8e2184ed8f6cfc5781d0a1afd68c3c392d575
cptbrkfst <dyl@resonategroup.ca>
2017-07-19 16:41:09 -0400

    added Mergers object to environment

--- lib/canadian_mergers.rb
+++ lib/canadian_mergers.rb
@@ -1,2 +1,3 @@
-require_relative "./canadian_mergers/version"
+require_relative './canadian_mergers/version'
 require_relative './canadian_mergers/cli'
+require_relative './canadian_mergers/mergers.rb'
