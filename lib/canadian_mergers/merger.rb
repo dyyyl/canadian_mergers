37935c29e63103b76e5868f49cbc25928c95c60a
cptbrkfst <dyl@resonategroup.ca>
2017-07-20 16:37:16 -0400

    allows for creation and class variable 'save'

--- lib/canadian_mergers/merger.rb
+++ lib/canadian_mergers/merger.rb
@@ -1,10 +1,27 @@
 # Merger object
 class CanadianMergers::Merger
+  @@all = []
   attr_accessor :parties, :industry, :result
 
   def initialize(parties, industry, result)
     @parties = parties
     @industry = industry
     @result = result
+  end
+
+  def save
+    self.class.all << self
+  end
+
+  def self.create(parties, industry, result)
+    new(parties, industry, result).tap(&:save)
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    all.clear
   end
 end
