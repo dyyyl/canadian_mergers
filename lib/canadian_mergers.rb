0b2ba9d14797f56a0e7e1ecf95a8d2d6b8c35175
cptbrkfst <dyl@resonategroup.ca>
2017-07-22 08:34:41 -0400

    dependency organization & csv

--- lib/canadian_mergers.rb
+++ lib/canadian_mergers.rb
@@ -1,10 +1,11 @@
+require 'csv'
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
+require 'terminal-table'
+
 require_relative './canadian_mergers/version'
 require_relative './canadian_mergers/cli'
 require_relative './canadian_mergers/merger_list.rb'
 require_relative './canadian_mergers/merger.rb'
 require_relative './canadian_mergers/scrape.rb'
-
-require 'nokogiri'
-require 'open-uri'
-require 'pry'
-require 'terminal-table'
