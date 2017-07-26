3d76747708ae3ff2904b06fcc961546fa200e466
cptbrkfst <dyl@resonategroup.ca>
2017-07-26 14:46:43 -0400

    removed redundant method

--- spec/canadian_mergers_spec.rb
+++ spec/canadian_mergers_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe CanadianMergers do
   it "has a version number" do
     expect(CanadianMergers::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
